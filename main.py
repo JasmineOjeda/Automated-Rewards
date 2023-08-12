f40a3f04c26c889e15c4495d5d8e1c7f73bd9235
JasmineOjeda <jasmineojeda01@gmail.com>
2023-08-12 14:20:07 -0700

    Initial mobile and desktop search algorithm

--- main.py
+++ main.py
@@ -4,21 +4,44 @@ import time
 import string
 import random
 
-options = webdriver.EdgeOptions()
-driver = webdriver.Edge(options=options)
+def search(driver, iterations):
+    driver.get("https://bing.com/")
+    time.sleep(3)
+    count = iterations
 
-driver.get("https://bing.com/")
-time.sleep(3)
+    while (count > 0) :
+        random_search = ''.join(random.choices(string.ascii_letters + " ", k=random.randint(3, 15)))
+        print(str(count) + ": " + random_search)
+        driver.find_element(By.CSS_SELECTOR, "[type='search']").send_keys(random_search)
+        time.sleep(0.5)
+        driver.find_element(By.CSS_SELECTOR, "[type='search']").send_keys("\n")
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, "[type='search']").clear()
+        time.sleep(0.5)
+        count -= 1
 
-##driver.get("https://rewards.bing.com/")
+def desktop_search():
+    options = webdriver.EdgeOptions()
+    driver = webdriver.Edge(options=options)
+    search(driver, 30)
+    driver.quit()
 
-count = 30
+def mobile_search():
+    options = webdriver.EdgeOptions()
+    options.use_chromium = True  
+    
+    mobile_emulation = {  
+        "deviceMetrics": { "width": 375, "height": 812, "pixelRatio": 3.0 },  
+        "userAgent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36 Edg/92.0.902.78"  
+    }  
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
 
-while (count > 0) :
-    random_search = ''.join(random.choices(string.ascii_letters + " ", k=random.randint(3, 15))) + "\n"
-    ##print(random_search)
-    driver.find_element(By.CSS_SELECTOR, "[type='search']").send_keys(random_search)
-    count-=1
-    time.sleep(random.randint(1, 3))
-    driver.find_element(By.CSS_SELECTOR, "[type='search']").clear()
+    driver = webdriver.Edge(options=options)
+    search(driver, 20)
+    driver.quit()
+
+desktop_search()
+mobile_search()
+
+#driver.get("https://rewards.bing.com/")
     
