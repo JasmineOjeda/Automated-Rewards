a51eb0add2e0f3a10ba386f13c4e0a0a41f90a2a
JasmineOjeda <jasmineojeda01@gmail.com>
2023-08-12 14:56:54 -0700

    Updated folders

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 import sys
 sys.path.insert(1, 'src')
 import search
+import activities
 
-search.desktop_search()
-search.mobile_search()
+options = webdriver.EdgeOptions()
+driver = webdriver.Edge(options=options)
 
-#driver.get("https://rewards.bing.com/")
+#search.desktop_search()
+#search.mobile_search()
+activities.daily_set(driver)
+
+time.sleep(3)
+driver.quit()
     
---
+++ src/activities.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import random
+
+def daily_set(driver):
+    driver.get("https://rewards.bing.com/")
